96e31678e8625dcfafc1b3470781bf0869fcb2f5
CharisHadi <charishadi@gmail.com>
2021-05-11 12:54:58 -0700

    Created main and JS file, made random rock/paper/scissors function for computer player

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hello fucker");
+
+function randomPlay(){
+    const plays = ["rock", "paper", "scissors"];
+    const rng = Math.floor(Math.random() *3);
+    return plays[rng];
+}
+
+function playRound(player1, player2){
+
+}
